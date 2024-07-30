1b6f1c7dc7af71de0f8d3b28d5362c964847730b
Nikhil Walia <nikhilwalia@Nikhils-MacBook-Air.local>
2024-07-30 12:29:22 +0530

    first

---
+++ src/main/java/com/chat/app/charRoomApp/CharRoomAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat.app.charRoomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharRoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharRoomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/app/charRoomApp/CharRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.app.charRoomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
